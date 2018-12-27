0f6ecd93ba3605f3636172d6ee5cb4c24a954078
ilushareg <ilushareg@gmail.com>
2018-12-27 13:30:05 +0300

    starting

---
+++ Assets/ShipControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+
+    public ParticleSystem engine = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        UpdateParticles();
+    }
+
+    void UpdateParticles()
+    {
+        if (engine == null)
+            return;
+
+        ParticleSystem.EmissionModule m = engine.emission;
+        m.rateOverTime = 100.0f;
+    }
+
+}
